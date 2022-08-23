eb22ccf8bc8702c313cad332c29417e9e70ffb62
肥龙啃锅盔 <1769811019@qq.com>
2022-08-23 06:54:55 +0000

    update common/const/var.go.
    Signed-off-by: 肥龙啃锅盔 <1769811019@qq.com>

--- common/const/var.go
+++ common/const/var.go
@@ -2,7 +2,7 @@ package _const
 
 // 定义常用数据
 var (
-	Nacos_Host         string = "1.14.60.160"
+	Nacos_Host         string = "127.0.0.1"
 	Nacos_Port         uint64 = 8848
 	Nacos_Names_PaceId string = "f49daff5-dbf9-437d-abb5-1431b8dcfc45"
 	Nacos_DataId       string = "application-dev.yaml"
